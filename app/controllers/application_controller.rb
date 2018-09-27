31f22313f8934b5b4ce808ca7cf56dd554f70d81
TheBlackFlame <muhammadhossain29@gmail.com>
2018-09-27 16:11:52 -0500

    updated form and controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-  params.require(:recipe).permit(:title, :description, ingredients_attributes:[:id, :content, :_destroy], steps_attributes:[:id, :direction, :_destroy])
+  
 end
